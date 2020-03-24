ea212be2f72627e6e6bee10227fcb2d890e8b835
Doren Proctor <dorenproctor@gmail.com>
2020-03-24 01:07:55 -0700

    remove semicolons works with comments
    rename injectSemicolons to be addSemicolons
    display line, column when failed to parse file
    add es2020.string to tsconfig to recognize str.matchAll
    no "curly": "warn" in eslintrc
    import and instantiate acorn at top of file
    add a couple missing ts types

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,28 @@
 import * as vscode from 'vscode';
-import { injectSemicolons, removeSemicolons } from './semicolonLogic';
+import { addSemicolons, removeSemicolons } from './semicolonLogic';
 
 function alterText(fn: Function) {
-	let editor = vscode.window.activeTextEditor;
-	if (editor) {
-		const fileContent = editor.document.getText();
-		const alteredContent = fn(fileContent);
-		const firstLine = editor.document.lineAt(0);
-		const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
-		const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
-		editor.edit(editBuilder => editBuilder.replace(textRange, alteredContent));
-	} else {
-		vscode.window.showInformationMessage('Failed to access current page');
+	try {
+		let editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const fileContent = editor.document.getText();
+			const alteredContent = fn(fileContent);
+			const firstLine = editor.document.lineAt(0);
+			const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+			const textRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+			editor.edit(editBuilder => editBuilder.replace(textRange, alteredContent));
+		} else {
+			vscode.window.showInformationMessage('Failed to access current page');
+		}
+	} catch(e) {
+		const {line, column} = e.loc;
+		vscode.window.showInformationMessage('Error parsing JavaScript at '+'line: '+line+', column: '+column);
 	}
 }
 
 export function activate(context: vscode.ExtensionContext) {
 	let addCommand = vscode.commands.registerCommand('extension.addSemicolons', () => {
-		alterText(injectSemicolons);
+		alterText(addSemicolons);
 	});
 	const removeCommand = vscode.commands.registerCommand('extension.removeSemicolons', () => {
 		alterText(removeSemicolons);
--- src/semicolonLogic.ts
+++ src/semicolonLogic.ts
@@ -1,7 +1,8 @@
-function injectSemicolons(code: string) {
+const {Parser} = require("acorn");
+const MyParser = Parser.extend(require("acorn-jsx")());
+
+function addSemicolons(code: string) {
   function getSemicolonSpots(code: string) : number[] { 
-    const {Parser} = require("acorn");
-    const MyParser = Parser.extend(require("acorn-jsx")());
     const spots: number[] = [];
     const options = {
       sourceType: 'module',
@@ -26,9 +27,35 @@ function injectSemicolons(code: string) {
 }
 
 function removeSemicolons(code: string) {
-  return code.replace(/;\s*\n/g, '\n').replace(/;\s*$/g, '');
+  function removeComments(code: string): string {
+    const len = code.length-1;
+    const options = {
+      sourceType: 'module',
+      onComment: function(block: boolean, text: string, start: number, end: number) {
+        code = code.substr(0, start) + ' '.repeat(end-start) + code.substr(end, len);
+      }
+    };
+    MyParser.parse(code, options);
+    return code;
+  }
+  
+  function findEndingSemicolonIndices(code: string): (number | undefined)[] {
+    return [...code.matchAll(/;\s*\n/g)].map(match => match.index);
+  }
+  
+  
+  function removeSemicolonsInIndices(code: string, indices: (number | undefined)[]): string {
+    for (let i=indices.length-1; i>=0; i--) {
+      const x = indices[i];
+      if (x) code = code.slice(0,x)+code.slice(x+1);
+    }
+    return code;
+  }
+
+  const noComments = removeComments(code);
+  const indices = findEndingSemicolonIndices(noComments);
+  return removeSemicolonsInIndices(code, indices);
 }
 
-export { injectSemicolons, removeSemicolons };
-export default { injectSemicolons, removeSemicolons };
-
+export { addSemicolons, removeSemicolons };
+export default { addSemicolons, removeSemicolons };
